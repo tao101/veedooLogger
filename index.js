5fb973a304aabc3f6747c9a8716d8b77d3d19f7e
taoufiq lotfi <leh.lotfi@gmail.com>
2023-02-16 21:16:28 +0100

    fix import issue

--- index.js
+++ index.js
@@ -7,11 +7,3 @@ export default {
 };
 
 export { Logger, getProjectIdByName };
-
-let logger = new Logger(
-  'veedoo.fibery.io',
-  'eff41760.156aab5142b8ca80d62fb6e6699fc2af8e3',
-  '1fbd2d20-9bb9-11ed-a56a-4ba07f01cc89'
-);
-
-logger.error('test');
